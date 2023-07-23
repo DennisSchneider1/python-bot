2f92eeadc2cabbc6153e2cf5b0f8a195efa12a1c
Dignity <dennis.schneider.nr.1@gmail.com>
2023-07-23 22:54:33 +0200

    Improve debug message

--- responses.py
+++ responses.py
@@ -2,7 +2,8 @@ import llmApi
 
 def get_response_for_user_input(user_name:str, message: str) -> str:
     if message.endswith('!debug\n'):
-        print(message)
+        print(f'This is the current chat history:\n{message}')
+        return 'no response'
 
     response = str(llmApi.llm_respond_creative_user(user_name=user_name, user_input=message))
     if len(response) >=1:
